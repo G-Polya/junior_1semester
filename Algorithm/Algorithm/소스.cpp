8fe50f71c7a063479e7afc5cd7dd980d19752fed
G_Polya <wlskaka4@gmail.com>
2020-03-19 21:45:29 +0900

    머농 0 0 0 9 45?

--- Algorithm/Algorithm/소스.cpp
+++ Algorithm/Algorithm/소스.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 
 // define GreedyBetterChange
-
 vector<int> GreedBetterChange(int M, vector<int> c, int d)
 {
 	vector<int> k(d, 0);
@@ -28,32 +27,18 @@ vector<int> GreedBetterChange(int M, vector<int> c, int d)
 vector<int> BruteForceChange(int M, vector<int> c, int d)
 {
 	vector<int> k(d, 0);
-	//reverse(begin(c), end(c));
-
+	
 	int smallestNumberOfCoins = INT32_MAX;
-	cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << endl;
-
+	
 	vector<int> bestchange(d, 0);
 
 	for (int i = 0; i < d; i++)
 	{
 
-		//	cout << "k[" << i << "] : " << k[i] << endl;
 		for (int m = k[i]; m <= M / c[i]; m++)
 		{
 			int valuesOfCoins = 0;
-			cout << "k[" << i << "], M/c[" << i << "] : " << m << ", " << M / c[i] << endl;
-			/*for (int j = 0; j < 5; j++)
-			{
-				cout << c[j] << " ";
-			}
-
-			cout << endl;
-*/
-
-
-//cout << "m: " << m <<" c: "<<c[i] << endl;
-
+			
 			valuesOfCoins += c[i] * m;
 
 
@@ -61,38 +46,27 @@ vector<int> BruteForceChange(int M, vector<int> c, int d)
 
 			if (valuesOfCoins == M)
 			{
-				cout << "valuesOfCoins: " << valuesOfCoins << endl << endl;
-
+				
 
 				int numberOfCoins = 0;
 				for (int n = 0; n <= m; n++)
 				{
 
 					numberOfCoins += n;
-					cout << "m : " << m << endl;
-					cout << "numberofCoins: " << numberOfCoins << endl;
-
+					
 				}
-				cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << ", numberofCoins: " << numberOfCoins << endl;
-
+				
 
 				if (numberOfCoins < smallestNumberOfCoins)
 				{
-					smallestNumberOfCoins = numberOfCoins;
-
-					cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << endl;
 					bestchange[i] = m;
 
 				}
 			}
 
 		}
-		cout << endl;
-		cout << "=========================" << endl;
 
-		//cout<<"\nvalue of coins : "<<valuesOfCoins << endl;
 	}
-	cout << endl;
 	return bestchange;
 }
 
