004cbaf21e9cdc02e6573fca7200b34d6b9de76a
G_Polya <wlskaka4@gmail.com>
2020-03-19 20:08:15 +0900

    dd

--- Algorithm/Algorithm/소스.cpp
+++ Algorithm/Algorithm/소스.cpp
@@ -31,87 +31,74 @@ vector<int> BruteForceChange(int M, vector<int> c, int d)
 	//reverse(begin(c), end(c));
 
 	int smallestNumberOfCoins = INT32_MAX;
-	//for (int k0 = 0; k0 < M / c[0]; k0++)
-	//{
-	//	for (int k1 = 0; k1 < M / c[1]; k1++)
-	//	{
-	//		for (int k2 = 0; k2 < M / c[2]; k2++)
-	//		{
-	//			for (int k3 = 0; k3 < M / c[3]; k3++)
-	//			{
-	//				for (int k4 = 0; k4 < M / c[4]; k4++)
-	//				{
-
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
+	cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << endl;
 
 	vector<int> bestchange(d, 0);
 
 	for (int i = 0; i < d; i++)
 	{
+
 		//	cout << "k[" << i << "] : " << k[i] << endl;
-		for (k[i]; k[i] < M / c[i]; k[i]++)
+		for (int m = k[i]; m <= M / c[i]; m++)
 		{
 			int valuesOfCoins = 0;
-
-			cout << "[" << i << "], M/c[" << i << "] : " << k[i] << ", " << M / c[i] << endl;
-			for (int j = 0; j < 5; j++)
+			cout << "k[" << i << "], M/c[" << i << "] : " << m << ", " << M / c[i] << endl;
+			/*for (int j = 0; j < 5; j++)
 			{
 				cout << c[j] << " ";
 			}
 
 			cout << endl;
-
-			for (int j = 0; j < 5; j++)
-			{
-				cout << k[j] << " ";
-			}
-			cout << endl;
+*/
 
 
-			for (int j = 0; j < 5; j++)
-			{
-				valuesOfCoins += c[j] * k[j];
+//cout << "m: " << m <<" c: "<<c[i] << endl;
 
-			}
-				cout <<  valuesOfCoins << endl;
+			valuesOfCoins += c[i] * m;
+
+
 
 
 			if (valuesOfCoins == M)
 			{
-				//cout << <<valuesOfCoins << endl;
+				cout << "valuesOfCoins: " << valuesOfCoins << endl << endl;
 
 
 				int numberOfCoins = 0;
-				for (int n = 0; n < d; n++)
+				for (int n = 0; n <= m; n++)
 				{
-					numberOfCoins += k[i];
+
+					numberOfCoins += n;
+					cout << "m : " << m << endl;
 					cout << "numberofCoins: " << numberOfCoins << endl;
 
 				}
+				cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << ", numberofCoins: " << numberOfCoins << endl;
+
+
 				if (numberOfCoins < smallestNumberOfCoins)
 				{
 					smallestNumberOfCoins = numberOfCoins;
-					k[i]++;
-					cout << smallestNumberOfCoins << " " << k[i] << endl;
+
+					cout << "smallestNumberOfCoin: " << smallestNumberOfCoins << endl;
+					bestchange[i] = m;
 
 				}
 			}
 
 		}
 		cout << endl;
+		cout << "=========================" << endl;
+
 		//cout<<"\nvalue of coins : "<<valuesOfCoins << endl;
 	}
 	cout << endl;
-	return k;
+	return bestchange;
 }
 
 int main()
 {
-	int M = 40;
+	int M = 45;
 	vector<int> c = { 25,20,10,5,1 };
 	int d = c.size();
 
