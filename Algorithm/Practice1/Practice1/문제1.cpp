d5213be3666f2c2572bfe68aa994c7d4228713a6
G_Polya <wlskaka4@gmail.com>
2020-03-22 18:01:02 +0900

    문제1번 주석추

--- Algorithm/Practice1/Practice1/문제1.cpp
+++ Algorithm/Practice1/Practice1/문제1.cpp
@@ -5,8 +5,9 @@
 #include <numeric>
 using namespace std;
 
+// 누적평균을 구하는 함수
 template<typename T>
-void accmulate_average(vector<T> vec)
+void accmulate_average(vector<T> vec)	//vector(동적배열)를 원소로 받는다
 {
 	double sum = 0;
 	cout << "Avr 출력" << endl;
@@ -24,10 +25,10 @@ void accmulate_average(vector<T> vec)
 
 int main()
 {
-	vector<int> vec;
+	vector<int> vec;	// vector(배열)
 	cout << "배열을 입력해주세요(입력을 종료하려면 Ctrl+Z): ";
-	copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(vec));
+	copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(vec)); // 입력
 	cout << endl << endl;
 	
-	accmulate_average(vec);
+	accmulate_average(vec);	// 출력
 }
