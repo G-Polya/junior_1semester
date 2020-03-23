ae854b6f15f860ae34c2f3ae1b6d3372c319eb7c
G_Polya <wlskaka4@gmail.com>
2020-03-23 16:34:25 +0900

    ?

--- JAVA/Algorithm_test/src/Test.java
+++ JAVA/Algorithm_test/src/Test.java
@@ -1,40 +1,26 @@
 
 public class Test
 {
-	
-	public int coinChange(int[] coins, int amount)
+	public int sum(int m, int n)
 	{
-		return coinChange(0, coins,amount);
+		return m + n;
 	}
 	
-	private static int coinChange(int idxCoin, int[] coins, int amount)
+	public static int add(int a, int b)
 	{
-		if(amount == 0)
-			return 0;
-		if(idxCoin < coins.length && amount>0)
-		{
-			int maxVal = amount / coins[idxCoin];
-			int minCost = Integer.MAX_VALUE;
-			for(int x = 0; x<= maxVal; x++)
-			{
-				if(amount >= x * coins[idxCoin])
-				{
-					int res = coinChange(idxCoin+1, coins,amount-x*coins[idxCoin]);
-					if(res!= -1)
-						minCost = Math.min(minCost, res + x);
-				}
-			}
-			return (minCost == Integer.MAX_VALUE) ? -1 : minCost;
-		}
-		return -1;
+		return a + b;
 	}
-	
-	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		int[] coins = {25,20,10,5,1};
-		System.out.println(coinChange(5, coins, 40));
+		Test test = new Test();
+		
+		int result1 = test.sum(3, 4);
+		int result2 = add(5,6);
+		int result3 = test.add(7, 8);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
 	}
 
 }
