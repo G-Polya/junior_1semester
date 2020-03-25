9298fd523ac34e0a74f46d5871635291f152f7b8
G_Polya <wlskaka4@gmail.com>
2020-03-25 21:39:43 +0900

    bubblesort

--- Algorithm/Practice 2/Practice 2/문제3.cpp
+++ Algorithm/Practice 2/Practice 2/문제3.cpp
@@ -8,19 +8,69 @@
 
 using namespace std;
 
+void swap(int &a, int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void bubbleSort(int arr[], int size)
+{
+	if (size == 1)
+		return;
+
+	for (int i = 0; i < size - 1; i++)
+		if (arr[i] > arr[i + 1])
+			swap(arr[i], arr[i+1]);
+
+	bubbleSort(arr, size - 1);
+}
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
 int main()
 {
 	srand((unsigned int)time(NULL));
-	array<int, 10> N;
-	array<int, 1000> K;
-
-	for (int i = 0; i < N.size(); i++)
+	
+	int* N = new int[10];
+	for (int i = 0; i < _msize(N)/sizeof(int); i++)
 		N[i] = rand() % 10000 + 1;
 
-	for (int i = 0; i < K.size(); i++)
+	int* K = new int[1000];
+	for (int i = 0; i < _msize(K) / sizeof(int); i++)
 		K[i] = rand() % 10000 + 1;
 
-	copy(begin(N), end(N), ostream_iterator<int>(cout, " "));
+
+	
+
+
 	
 	cout << endl;
 
