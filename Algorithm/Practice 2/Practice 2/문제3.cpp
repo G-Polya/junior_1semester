580b3b3cab6e0d92018d6a1fe4e3c2f3759bf578
G_Polya <wlskaka4@gmail.com>
2020-03-27 12:51:58 +0900

    problem 3 complete

--- Algorithm/Practice 2/Practice 2/문제3.cpp
+++ Algorithm/Practice 2/Practice 2/문제3.cpp
@@ -119,23 +119,20 @@ int main()
 	//printArray(N, N_size);
 	//cout << endl;
 
-	//3-4
-	
-	
+	//3-4	
 	chrono::steady_clock::time_point bubble_begin = chrono::steady_clock::now();
 	bubbleSort(K, K_size);
 	chrono::steady_clock::time_point bubble_end = chrono::steady_clock::now();
 	auto bubble_elapsed_time = chrono::duration_cast<chrono::microseconds>(bubble_end - bubble_begin).count();
-
+	
 	chrono::steady_clock::time_point quick_begin = chrono::steady_clock::now();
-	quickSort(K, 0, K_size - 1);
+	quickSort(copy_K, 0, K_size - 1);
 	chrono::steady_clock::time_point quick_end = chrono::steady_clock::now();
 	auto quick_elapsed_time = chrono::duration_cast<chrono::microseconds>(quick_end - quick_begin).count();
 
-
 	
-	cout << "Elapsed time of Bubble sort: " << bubble_elapsed_time<<" [us]"<< endl;
-	cout << "Elapsed time of Quick sort: " << quick_elapsed_time <<" [us]" <<endl;
+	cout << "Elapsed time of Bubble sort: " << bubble_elapsed_time<<" us"<< endl;
+	cout << "Elapsed time of Quick sort: " << quick_elapsed_time <<" us" <<endl;
 
 
 	delete[] N;
