103b5e3f0b757c2b54ab9ec2c12997027d3648b4
G_Polya <wlskaka4@gmail.com>
2020-03-27 14:54:25 +0900

    complete

--- Algorithm/Practice 2/Practice 2/문제3.cpp
+++ Algorithm/Practice 2/Practice 2/문제3.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+
+// swap function
 void swap(int &a, int &b)
 {
 	int temp;
@@ -17,18 +19,21 @@ void swap(int &a, int &b)
 	b = temp;
 }
 
+
+// bubble sort
 void bubbleSort(int arr[], int size)
 {
 	if (size == 1)
 		return;
 
 	for (int i = 0; i < size - 1; i++)
-		if (arr[i] > arr[i + 1])
+		if (arr[i] < arr[i + 1])
 			swap(arr[i], arr[i+1]);
 
 	bubbleSort(arr, size - 1);
 }
 
+// partition for quick sort
 int partition(int arr[], int low, int high)
 {
 	int pivot = arr[high];
@@ -36,7 +41,7 @@ int partition(int arr[], int low, int high)
 
 	for (int j = low; j <= high - 1; j++)
 	{
-		if (arr[j] < pivot)
+		if (arr[j] > pivot)
 		{
 			i++;
 			swap(arr[i], arr[j]);
@@ -46,6 +51,7 @@ int partition(int arr[], int low, int high)
 	return (i + 1);
 }
 
+// quick sort
 void quickSort(int arr[], int low, int high)
 {
 	int* stack = new int[high - low + 1];
@@ -87,44 +93,74 @@ void printArray(int arr[], int size)
 
 int main()
 {
-	srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));			// random시드 설정
 	
-	int* N = new int[10];						// 3-2번 버블소트용 배열
+	int* N = new int[10];						// 3-2번 버블정렬용 배열
 	int N_size = _msize(N) / sizeof(int);
-	int* copy_N = new int[N_size];				// 3-3번 퀵소트용 배열
+	int* copy_N = new int[N_size];				// 3-3번 퀵정렬용 배열
+	
+	// 배열 초기화
 	for (int i = 0; i < N_size; i++)
-		N[i] = rand() % 10000 + 1;
+	{
+		int* rand_number = new int(rand()%10000+1);
+		N[i] = *rand_number;
+		copy_N[i] = *rand_number;
+		delete rand_number;
+	}
+
+	int* K = new int[1000];						// 버블정렬용 배열
+	int K_size = _msize(K) / sizeof(int);
+	int* copy_K = new int[K_size];				// 퀵정렬용 배열
+
+
+	// 배열 초기화
+	for (int i = 0; i < K_size; i++)
+	{
+		int* rand_number = new int(rand() % 10000 + 1);
+		K[i] = *rand_number;
+		copy_K[i] = *rand_number;
+		delete rand_number;
+	}
+
+
 
 	// 3-1
 	cout << "Original array N: ";
 	printArray(N, N_size);
 	
-	int* K = new int[1000];						// 버블소트용 배열
-	int K_size = _msize(K) / sizeof(int);
-	int* copy_K = new int[K_size];				// 퀵소트용 배열
-	copy(K, K + K_size, copy_K);
-	for (int i = 0; i < K_size; i++)
-		K[i] = rand() % 10000 + 1;
+	
+	
 
 	// 3-2
+	
 	cout << "Bubble Sort: ";
 	bubbleSort(N, N_size);
 	printArray(N, N_size);
 	
 
+	cout << endl;
 	// 3-3
+	cout << "Copied array copy_N: ";
+	printArray(copy_N, N_size);
 	cout << "Quick Sort: ";
 	quickSort(copy_N, 0, N_size-1);
 	printArray(N, N_size);
 	cout << endl;
-	cout << endl;
+	
+	cout << "====================================\n" << endl;
+
+	
 
 	//3-4	
+
+	// 버블 정렬
 	chrono::steady_clock::time_point bubble_begin = chrono::steady_clock::now();
 	bubbleSort(K, K_size);
 	chrono::steady_clock::time_point bubble_end = chrono::steady_clock::now();
 	auto bubble_elapsed_time = chrono::duration_cast<chrono::microseconds>(bubble_end - bubble_begin).count();
 	
+
+	// 퀵 정렬
 	chrono::steady_clock::time_point quick_begin = chrono::steady_clock::now();
 	quickSort(copy_K, 0, K_size - 1);
 	chrono::steady_clock::time_point quick_end = chrono::steady_clock::now();
