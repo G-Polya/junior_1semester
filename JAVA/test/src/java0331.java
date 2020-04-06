698cc0a41d2963011958885918d84809c1fc0b7e
G_Polya <wlskaka4@gmail.com>
2020-04-06 16:10:51 +0900

    ?

--- JAVA/test/src/java0331.java
+++ JAVA/test/src/java0331.java
@@ -1,9 +1,20 @@
+class StaticMethod
+{
+	public int m;
+	static int add(int b)
+	{
+		return m + b;
+	}
+}
+
 
 public class java0331
 {
 	public static void main(String[] args)
 	{
-		
+		StaticMethod a = new StaticMethod();
+		a.m = 4;
+		System.out.println(a.add(5));
 	}
 
 }
