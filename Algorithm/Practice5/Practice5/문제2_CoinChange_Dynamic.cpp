109e2e66250d92f84417cb47aafbd1bac5d0cd6e
G_Polya <wlskaka4@gmail.com>
2020-04-17 19:27:15 +0900

    dd

--- Algorithm/Practice5/Practice5/문제2_CoinChange_Dynamic.cpp
+++ Algorithm/Practice5/Practice5/문제2_CoinChange_Dynamic.cpp
@@ -13,17 +13,54 @@ void printArray(vector<int> arr)
 	cout << endl;
 }
 
+// 최소의 동전 개수 반환
+int minCoins(vector<int> coins, int amount)
+{
+	// base
+	if (amount == 0)
+		return 0;
+
+	int res = INT_MAX;
+
+	for (int i = 0; i < coins.size(); i++)				
+	{
+		if (coins[i] <= amount)
+		{
+			int sub_res = minCoins(coins, amount - coins[i]);
+		
+			if (sub_res != INT_MAX && sub_res + 1 < res)
+				res = sub_res + 1;
+		}
+	}
+
+	return res;
+}
+
 void Dynamic_CoinChange(vector<int> arr, int amount)
 {
+	// 1~11을 key로 갖는 딕셔너리 생성, 초기화
 	map<int, int> amount_dict;
 	for (int i = 1; i < amount + 1; i++)
 	{
-		amount_dict.insert(make_pair(i, 0));
+		amount_dict.insert(make_pair(i, minCoins(arr, i)));
 	}
 	
-	for (auto it = amount_dict.begin(); it != amount_dict.end(); it++) {
-		cout << "key : " << it->first << " " << "value : " << it->second << '\n';
+
+
+	// 출력
+	cout << "Keys:  ";
+	for (auto it = amount_dict.begin(); it != amount_dict.end(); it++)
+	{
+		cout << setw(3)<<it->first << " " ;
 	}
+	cout << endl;
+	cout << "Values:";
+	for (auto it = amount_dict.begin(); it != amount_dict.end(); it++)
+	{
+		cout<< setw(3) << it->second << " ";
+	}
+	cout << endl;
+	
 }
 
 int main()
@@ -31,5 +68,6 @@ int main()
 	vector<int> coins = { 1,4,7 };
 	int amount = 11;
 
+	
 	Dynamic_CoinChange(coins, amount);
 }
