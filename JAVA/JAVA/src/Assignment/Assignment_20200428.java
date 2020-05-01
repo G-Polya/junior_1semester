2c9d7d00ca0999a3b2df68d9223c1bc4b6aa4d0d
G_Polya <wlskaka4@gmail.com>
2020-05-01 16:30:55 +0900

    java chapter 6 assignment

--- JAVA/JAVA/src/Assignment/Assignment_20200428.java
+++ JAVA/JAVA/src/Assignment/Assignment_20200428.java
@@ -2,42 +2,45 @@ package Assignment;
 
 import java.util.StringTokenizer;
 import java.util.Scanner;
-import java.io.*;
-
-class StringTokenizing
-{
-	public static void run(String str)
-	{
-		StringTokenizer st = new StringTokenizer(str, " ={};\t\r\n,", true);
-		int n = st.countTokens();
-		
-		int i = 0;
-		String[] tokens = new String[n];
-		while(st.hasMoreTokens())
-		{
-			tokens[i++] = st.nextToken();
-		}
-	}
-	
-}
 
 public class Assignment_20200428 
 {
 	
 	public static void main(String[] args)
-	{
-		Scanner scanner = new Scanner(System.in);
-		String str = "";
-		
-		while(true)
-		{
-			String input = scanner.nextLine();
-			if(input.contentEquals(("@"))
-					break;
-			else
-			{
-				str = str.concat(input+"\n");
-			}
-		}
-	}
+    {
+        Scanner sc = new Scanner(System.in);			// Scanner 객체 생성
+        String input_str = "";		// input_str 초기화
+
+        System.out.println("Input($로 입력 종료) >> ");
+        while(true)
+        {
+            String input = sc.nextLine();	// 문자열 한줄씩 입력
+            if(input.contentEquals("$"))	// while문 종료를 위한 flag $	
+                break;
+            else
+                input_str = input_str.concat(input+"\n");	//완성된 문자열
+            
+        }
+        
+        StringTokenizer st = new StringTokenizer(input_str, " ={};\t\r\n,");	// input_str을 StringTokenizer로 토큰화
+        int index = 0, temp = 0;
+        
+        String[] tokens = new String[st.countTokens()]; // 토큰들이 들어가는 배열 tokens
+        
+        System.out.println("Output >> ");
+        while(st.hasMoreTokens())
+        	tokens[index++] = st.nextToken();		// 배열 초기화.. 이제 tokens에는 input_str을 토큰화한 값들이 들어감
+        
+        for(int i = 0; i< tokens.length;i++)
+        {
+        	try
+        	{
+        		temp = Integer.parseInt(tokens[i]);	// 토큰 중에서 정수("1", "6")는 변환이 되지만 문자열("Example", "Example2")는 변환이 안돼서 NumberFormatException 발생
+        	}
+        	catch(NumberFormatException e)			
+        	{
+        		System.out.println(tokens[i]);		// 숫자 리터럴을 제외한 값들만 출력
+        	}
+        }
+    }
 }
--- JAVA/JAVA/src/Assignment/test.java
+++ JAVA/JAVA/src/Assignment/test.java
@@ -2,27 +2,34 @@ package Assignment;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class test 
+
+public class test
 {
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		String input="";
-		
-		while(sc.hasNextLine()) 
-		{
-			input= sc.nextLine();
-			
-			StringTokenizer st = new StringTokenizer(input, " ={};\t\r\n,", true);
-			//System.out.println("Input:"+ input);
-			while(st.hasMoreTokens())
-				System.out.println(st.nextToken());
-		
-			
-		}
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        String input_str = "";
+//        StringTokenizing stz2 =new StringTokenizing();
 
-	
-	}
+        System.out.println("Input($로 입력 종료) >> ");
+        while(true)
+        {
+            String input = sc.nextLine();
+            if(input.contentEquals("$"))
+            {
+                break;
+            }
+            else
+            {
+                input_str = input_str.concat(input+"\n");
+            }
+        }
 
+        //    stz2.run(input_str);
+        StringTokenizer st = new StringTokenizer(input_str, " ={};\t\r\n,");
+        System.out.println("Output >> ");
+        while(st.hasMoreTokens())
+            System.out.println(st.nextToken()) ;
+    }
 }
