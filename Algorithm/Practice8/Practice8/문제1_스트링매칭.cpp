1f89b890c4a140aba09f4fb44510d0c813440cda
G_Polya <wlskaka4@gmail.com>
2020-05-06 21:54:55 +0900

    sds

--- Algorithm/Practice8/Practice8/문제1_스트링매칭.cpp
+++ Algorithm/Practice8/Practice8/문제1_스트링매칭.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <string>
 #include <iterator>
+#include <cmath>
 
 using namespace std;
 
@@ -22,9 +23,26 @@ void brute_force_matching(string text, string pattern)
 	}
 }
 
-void KMP_matching(string text, string pattern)
+//https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
+// q: 해시함수에 의해 결정되는 mod를 위한 제수
+void rabin_karp_matching(string text, string pattern, int q)
 {
-	const int d = 36; // [0~9]+[a-z] = 10 +_ 26 = 36
+	const int d = 36; // [0~9]+[a-z] = 10 +_ 26 = 36. 
+	const int M = pattern.size();
+	const int N = text.size();
+	int p = 0;		// pattern을 위한 hash 값
+	int t = 0;		// text를 위한 hash값
+	
+	int h = (int)pow(d, M-1) % q;
+
+	// pattern과 text의 첫번째 덩이의 hash 계산 (pattern길이만큼 text를 잘라놓은 거를 '덩이'라고 하겠다.)
+	for (int i = 0; i < M; i++)
+	{
+		p = (d * p + pattern[i]) % q;
+		t = (d * t + text[i]) % q;
+	}
+
+
 
 }
 
