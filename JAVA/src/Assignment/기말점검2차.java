7b0b31a6eab38ffff0e13c7ffe413c9838697dff
G_Polya <wlskaka4@gmail.com>
2020-05-12 22:14:33 +0900

    no message

--- JAVA/src/Assignment/기말점검2차.java
+++ JAVA/src/Assignment/기말점검2차.java
@@ -1,15 +1,17 @@
 package Assignment;
-import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.*;
 
 class Student
 {
 	private String id_num;
 	private String name;
-	private int attendance;
-	private int mid_term;
-	private int final_term;
+	private String attendance;
+	private String mid_term;
+	private String final_term;
 	
-	public Student(String id_num, String name, int attendance, int mid_term, int final_term)
+	public Student(String id_num, String name, String attendance, String mid_term, String final_term)
 	{
 		this.id_num = id_num;
 		this.name = name;
@@ -28,17 +30,17 @@ class Student
 		return name;
 	}
 	
-	public int get_Attendance()
+	public String get_Attendance()
 	{
 		return attendance;
 	}
 	
-	public int get_Midterm()
+	public String get_Midterm()
 	{
 		return mid_term;
 	}
 	
-	public int get_Fianlterm()
+	public String get_Fianlterm()
 	{
 		return final_term;
 	}
@@ -46,6 +48,32 @@ class Student
 
 public class 기말점검2차 
 {
+	public static void createCSV(List<Student> list, String title, String filepath)
+	{
+		//int resultCount = 0;
+		try
+		{
+			BufferedWriter fw = new BufferedWriter(new FileWriter(filepath+"\\"+title+".csv", true));
+			
+			
+			fw.write("학번,이름,출석,중간,기말");
+			
+			for (Student student : list)
+			{
+				fw.write(student.get_Id()+","+student.get_Name() +","+student.get_Attendance()+","+student.get_Midterm()+","+student.get_Fianlterm());
+				fw.newLine();
+				//resultCount++;
+			}
+		
+			fw.flush();
+			fw.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
@@ -53,28 +81,25 @@ public class 기말점검2차
 		System.out.print("학생 수를 입력하세요 >> ");
 		int size = scanner.nextInt();
 		
+		List<Student> student_list = new ArrayList<Student>();
 		
-		Student[] students = new Student[size];
-		boolean flag = true;
-		System.out.println("학번, 성명, 출석, 과제, 중간, 기말 순으로 20명 이상 입력하세요(stop을 입력하면 그만입력합니다)");
-		for(int i = 0; i< students.length; i ++)
-		{	String id_num = scanner.next();
-			if(id_num.equals("stop"))
-				flag = false;
-			
-			String name = scanner.next();
-			int attendance = scanner.nextInt();
-			int mid_term = scanner.nextInt();
-			int final_term = scanner.nextInt();
-			
-			students[i] = new Student(id_num, name, attendance, mid_term, final_term);
-			
-		}
+		System.out.println("학번, 성명, 출석, 과제, 중간, 기말 순으로 20명 이상 입력하세요");
+		//Student first = new Student("학번","이름","출석","중간","기말");
+		//student_list.add(first);
 		
-		for(int k = 0; k<students.length;k++)
+		for(int i = 0; i<size;i++)
 		{
-			System.out.println(students[k].get_Attendance());
+			String id_num = scanner.next();
+			String name = scanner.next();
+			String attendance = scanner.next();
+			String mid_term = scanner.next();
+			String final_term = scanner.next();
+			
+			Student student = new Student(id_num, name, attendance, mid_term, final_term);
+			student_list.add(student);
 		}
+		
+		기말점검2차.createCSV(student_list, "Students","C:\\Users\\wlska\\Documents\\junior_1semester\\JAVA\\src\\Assignment" );	
 	}
 	
 }
