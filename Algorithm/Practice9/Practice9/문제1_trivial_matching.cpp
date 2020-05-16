1cc5c571d06d71faf26feb3f387164ba53a114a5
G_Polya <wlskaka4@gmail.com>
2020-05-16 19:26:36 +0900

    no message

--- Algorithm/Practice9/Practice9/문제1_trivial_matching.cpp
+++ Algorithm/Practice9/Practice9/문제1_trivial_matching.cpp
@@ -8,6 +8,8 @@
 #include <queue>
 using namespace std;
 
+
+//referenceDNA를 생성하는 함수
 void make_refDNA(string filename,int length)
 {
 	ofstream fout;		//파일객체 생성
@@ -32,6 +34,8 @@ void make_refDNA(string filename,int length)
 	fout.close();
 }
 
+
+// 매개변수로 주어진 배열에서 임의의 원소를 랜덤하게 추출하는 함수
 template <typename T>
 T shuffle(T arr[], int num)
 {
@@ -49,6 +53,7 @@ T shuffle(T arr[], int num)
 	return temp;
 }
 
+// string배열의 원소들을 이어붙인 string을 반환하는 함수 
 string concat(string strArr[], int size)
 {
 	string result;
@@ -66,7 +71,7 @@ string concat(string strArr[], int size)
 	return result;
 }
 
-void make_shortRead(int length, int n,string refDNA)
+void make_shortRead(int length, int n,string refDNA,string directory)
 {
 	ifstream fin;	// 읽기 파일 객체 생성
 	fin.open(refDNA);
@@ -78,93 +83,122 @@ void make_shortRead(int length, int n,string refDNA)
 	
 	uniform_int_distribution<int> dis(0, text.length()-length);		// 시작위지를 랜덤으로 설정
 
-	uniform_int_distribution<int> sample(0,4);	//20%확률로
+	uniform_int_distribution<int> sample(0,4);	//20%확률로 뽑아올 인덱스
 
 	for (int i = 0; i < n; i++)
 	{
 		ofstream fout;
-		string short_Name = "shortRead\shortRead_" + to_string(i) + ".txt";
+		string short_Name = directory+"\\shortRead_" + to_string(i) + ".txt";	//shortRead디렉토리에 저장
 		fout.open(short_Name);
-		std::cout << dis(gen) << endl;
-		string temp = text.substr(dis(gen), length);
+		//std::cout << dis(gen) << endl;
 
-		string* splited_temp = new string[length / 5];
-		int size = length / 5;
+		string temp = text.substr(dis(gen), length);	// 랜덤으로 뽑은 시작위치에서 length만큼의 서브스트링 반환
+
+		string* splited_temp = new string[length / 5];		// 20%확률로 돌연변이가 일어나기 위해 temp를 잘라서 만든 string배열
+		int size = length / 5;		// 예를 들어 length가 10이라면 splited_temp의 원소개수는 2개이다
 
 		for (int i = 0; i < size; i++)
 		{
-			splited_temp[i] = temp.substr(i * (length / size), length / size);
+			splited_temp[i] = temp.substr(i * (length / size), length / size);	// splited_temp에는 temp를 등분한 string이 저장됨 
 		}
 
-		std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
+		//std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)			//splited_temp의 모든 원소. 즉 temp가 등분되어 만들어진 string에 대하여
 		{
-			switch (splited_temp[i][sample(gen)])
-			{
-			case 'A':
+			int index = sample(gen);		// 20%확률로 랜덤하게 돌연변이가 일어날 인덱스 설정
+			
+			if (splited_temp[i][index] == 'A')	// 돌연변이가 일어날 인덱스에 해당하는 문자가 A라면
 			{
 				char arr[3] = { 'G','C','T' };
-				std::cout << sample(gen) << endl;
-				std::cout << splited_temp[i][sample(gen)] << endl;
-				splited_temp[i][sample(gen)] = shuffle(arr, 3);
-				continue;
+				splited_temp[i][index] = shuffle(arr, 3);	// G,C,T 중에서 하나로 바뀜
 			}
-			case 'G':
+			else if (splited_temp[i][index] == 'G')
 			{
 				char arr[3] = { 'A','C','T' };
-				splited_temp[i][sample(gen)] = shuffle(arr, 3);
-				continue;
+				splited_temp[i][index] = shuffle(arr, 3);
+
 			}
-			case 'C':
+			else if (splited_temp[i][index] == 'C')
 			{
 				char arr[3] = { 'A','G','T' };
-				splited_temp[i][sample(gen)] = shuffle(arr, 3);
-				continue;
+				splited_temp[i][index] = shuffle(arr, 3);
+
 			}
-			case 'T':
+			else if (splited_temp[i][index] == 'T')
 			{
-				char arr[3] = { 'A','C','G' };
-				splited_temp[i][sample(gen)] = shuffle(arr, 3);
-				continue;
-			}
+				char arr[3] = { 'A','G','C' };
+				splited_temp[i][index] = shuffle(arr, 3);
 
 			}
 		}
-		//std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
 		
-		string shortRead = concat(splited_temp, size);
-		string* splited_Read = new string[size];
-		for (int i = 0; i < size; i++)
-		{
-			splited_Read[i] = shortRead.substr(i * (length / size), length / size);
-		}
-		std::cout << shortRead<< " " << splited_Read[0] << " " << splited_Read[1] << endl;
+		string shortRead = concat(splited_temp, size);	//	돌연변이가 일어난 결과. shortRead
 
-		std::cout << "==============================" << endl;
-		
-		
+		//string* splited_Read = new string[size];
+		//for (int i = 0; i < size; i++)
+		//{
+		//	splited_Read[i] = shortRead.substr(i * (length / size), length / size);
+		//}
+
+		//std::cout << shortRead << " " << splited_Read[0] << " " << splited_Read[1] << endl;
+		//
+		//std::cout << "============================" << endl;
+		fout << shortRead << endl;
+		fout.close();
 	}
+	
+
+}
+
+void trivial_Mapping(string shortRead, string refDNA, int threshold)
+{
+	int M = shortRead.size();
+	int N = refDNA.size();
+	int count = 0;
+	for (int i = 0; i <= N - M; i++)
+	{
+		int j;
+		for (j = 0; j < M; j++)
+			if (shortRead[j] != refDNA[i + j])
+			{
+				int mismatch = 0;
+				mismatch++;
+				if (mismatch > threshold)
+				{
+					cout << "Total of " << mismatch << " mismatches. Not below threshold" << endl;
+					break;
+				}
+				else
+					count = mismatch;
+			}	
+	}
+	cout << "Total of " << count <<" mismatches. Below threshold"<< endl;
 }
 
 
 int main()
 {
-	//make_refDNA("refernceDNA.txt", 500000);
-	int length = 10;
-	make_shortRead(length, 30, "refernceDNA.txt");
-	//
-	//string shortRead;
-	//for (int i = 0; i < length; i++)
-	//{
-	//	ifstream fin;
-	//	fin.open("shortRead_" + to_string(i) + ".txt", ios::app | ios::out);
-	//	string shortRead;
-	//	fin >> shortRead;
+	//make_refDNA("referenceDNA.txt", 500000);
+	int length = 30;
+	make_shortRead(length, 20000, "referenceDNA.txt","30_shortRead");
+	ifstream inDNA;
+	inDNA.open("referenceDNA.txt");
+	string refDNA;
+	inDNA >> refDNA;
+	//cout << refDNA << endl;
 
-	//}
+	string shortRead;
+	for (int i = 0; i < length; i++)
+	{
+		ifstream fin;
+		fin.open("30_shortRead\\shortRead_" + to_string(i) + ".txt");
+		string shortRead;
+		fin >> shortRead;
+		
+		trivial_Mapping(shortRead, refDNA, 3);
 
-	string strArr[] = { "1","23","4562" };
-	string result = concat(strArr, 3);
+	}
+
 	
 }
