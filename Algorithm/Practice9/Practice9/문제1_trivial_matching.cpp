5ecf1f2cca88e3f95a99a03c5867a6705adf9074
G_Polya <wlskaka4@gmail.com>
2020-05-16 17:49:39 +0900

    no message

--- Algorithm/Practice9/Practice9/문제1_trivial_matching.cpp
+++ Algorithm/Practice9/Practice9/문제1_trivial_matching.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <random>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
 using namespace std;
 
 void make_refDNA(string filename,int length)
@@ -29,6 +32,40 @@ void make_refDNA(string filename,int length)
 	fout.close();
 }
 
+template <typename T>
+T shuffle(T arr[], int num)
+{
+	srand((unsigned int)time(NULL));
+	T temp;
+	int rn;
+	for (int i = 0; i < num - 1; i++)
+	{
+		rn = rand() % (num - i) + i;
+		temp = arr[i];
+		arr[i] = arr[rn];
+		arr[rn] = temp;
+	}
+
+	return temp;
+}
+
+string concat(string strArr[], int size)
+{
+	string result;
+
+	queue<string> q;
+	for (int i = 0; i < size; i++)
+		q.push(strArr[i]);
+
+	while (!q.empty())
+	{
+		result += q.front();
+		q.pop();
+	}
+
+	return result;
+}
+
 void make_shortRead(int length, int n,string refDNA)
 {
 	ifstream fin;	// 읽기 파일 객체 생성
@@ -41,52 +78,93 @@ void make_shortRead(int length, int n,string refDNA)
 	
 	uniform_int_distribution<int> dis(0, text.length()-length);		// 시작위지를 랜덤으로 설정
 
-	uniform_int_distribution<int> sample(0,5);	//20%확률로
+	uniform_int_distribution<int> sample(0,4);	//20%확률로
+
 	for (int i = 0; i < n; i++)
 	{
 		ofstream fout;
 		string short_Name = "shortRead\shortRead_" + to_string(i) + ".txt";
 		fout.open(short_Name);
-		cout << dis(gen) << endl;
+		std::cout << dis(gen) << endl;
 		string temp = text.substr(dis(gen), length);
-		
-		string splited_temp1 = temp.substr(0, length / 2);
-		string splited_temp2 = temp.substr(length / 2, length / 2);
-		cout << temp << " "<< splited_temp1 <<" "<< splited_temp2 << endl;
-		
-		switch (splited_temp1[sample(gen)])
+
+		string* splited_temp = new string[length / 5];
+		int size = length / 5;
+
+		for (int i = 0; i < size; i++)
 		{
-		case 'A':
-			splited_temp1[sample(gen)] = 'G';
-			cout << " dd " << splited_temp1 << endl;
+			splited_temp[i] = temp.substr(i * (length / size), length / size);
 		}
 
-		string shortRead;
-		
-		
-		//cout << "temp : " << temp << ", shortRead : " << shortRead << endl;
-		fout << temp;
-		fout.close();
+		std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
 
+		for (int i = 0; i < size; i++)
+		{
+			switch (splited_temp[i][sample(gen)])
+			{
+			case 'A':
+			{
+				char arr[3] = { 'G','C','T' };
+				std::cout << sample(gen) << endl;
+				std::cout << splited_temp[i][sample(gen)] << endl;
+				splited_temp[i][sample(gen)] = shuffle(arr, 3);
+				continue;
+			}
+			case 'G':
+			{
+				char arr[3] = { 'A','C','T' };
+				splited_temp[i][sample(gen)] = shuffle(arr, 3);
+				continue;
+			}
+			case 'C':
+			{
+				char arr[3] = { 'A','G','T' };
+				splited_temp[i][sample(gen)] = shuffle(arr, 3);
+				continue;
+			}
+			case 'T':
+			{
+				char arr[3] = { 'A','C','G' };
+				splited_temp[i][sample(gen)] = shuffle(arr, 3);
+				continue;
+			}
+
+			}
+		}
+		//std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
+		
+		string shortRead = concat(splited_temp, size);
+		string* splited_Read = new string[size];
+		for (int i = 0; i < size; i++)
+		{
+			splited_Read[i] = shortRead.substr(i * (length / size), length / size);
+		}
+		std::cout << shortRead<< " " << splited_Read[0] << " " << splited_Read[1] << endl;
+
+		std::cout << "==============================" << endl;
+		
+		
 	}
-		
-
 }
+
 
 int main()
 {
 	//make_refDNA("refernceDNA.txt", 500000);
 	int length = 10;
 	make_shortRead(length, 30, "refernceDNA.txt");
+	//
+	//string shortRead;
+	//for (int i = 0; i < length; i++)
+	//{
+	//	ifstream fin;
+	//	fin.open("shortRead_" + to_string(i) + ".txt", ios::app | ios::out);
+	//	string shortRead;
+	//	fin >> shortRead;
+
+	//}
+
+	string strArr[] = { "1","23","4562" };
+	string result = concat(strArr, 3);
 	
-	string shortRead;
-	for (int i = 0; i < length; i++)
-	{
-		ifstream fin;
-		fin.open("shortRead_" + to_string(i) + ".txt", ios::app | ios::out);
-		string shortRead;
-		fin >> shortRead;
-
-	}
-
 }
