a7918233df10b12ed0830eb6a1cac514a0c5ae50
G_Polya <wlskaka4@gmail.com>
2020-05-18 14:29:23 +0900

    no message

--- Algorithm/Practice9/Practice9/temp.cpp
+++ Algorithm/Practice9/Practice9/temp.cpp
@@ -75,6 +75,57 @@ string concat(string strArr[], int size)
 	return result;
 }
 
+// 
+string make_myDNA(string refDNA)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> sample(0, 4);
+	int col = 5;
+	int row = refDNA.length() / col;
+
+
+	//myDNA를 5씩 나눔. 20%확률을 위해 
+	string* temp_myDNA = new string[row];
+	for (int i = 0; i < row; i += col)
+	{
+		temp_myDNA[i] = refDNA.substr(i * col, col);
+	}
+
+	for (int i = 0; i < row; i++)			//temp_myDNA의 모든 원소. 즉 myDNA가 등분되어 만들어진 string에 대하여
+	{
+		int index = sample(gen);		// 20%확률로 랜덤하게 돌연변이가 일어날 인덱스 설정
+
+		if (temp_myDNA[i][index] == 'A')	// 돌연변이가 일어날 인덱스에 해당하는 문자가 A라면. 
+		{
+			char arr[3] = { 'G','C','T' };
+			temp_myDNA[i][index] = shuffle(arr, 3);	// G,C,T 중에서 하나로 바뀜
+		}
+		else if (temp_myDNA[i][index] == 'G')
+		{
+			char arr[3] = { 'A','C','T' };
+			temp_myDNA[i][index] = shuffle(arr, 3);
+
+		}
+		else if (temp_myDNA[i][index] == 'C')
+		{
+			char arr[3] = { 'A','G','T' };
+			temp_myDNA[i][index] = shuffle(arr, 3);
+
+		}
+		else if (temp_myDNA[i][index] == 'T')
+		{
+			char arr[3] = { 'A','G','C' };
+			temp_myDNA[i][index] = shuffle(arr, 3);
+
+		}
+	}
+
+	string myDNA = concat(temp_myDNA, row);
+
+	return myDNA;
+}
+
 void make_shortRead(int length, int n, string refDNA, string directory)
 {
 	ifstream fin;	// 읽기 파일 객체 생성
@@ -85,70 +136,21 @@ void make_shortRead(int length, int n, string refDNA, string directory)
 	random_device rd;	// 시드값을 얻기 위한 random_device 설정
 	mt19937 gen(rd());
 
-	uniform_int_distribution<int> dis(0, text.length() - length);		// 시작위지를 랜덤으로 설정
+	string myDNA = make_myDNA(text);
+	uniform_int_distribution<int> dis(0, myDNA.length() - length);		// 시작위지를 랜덤으로 설정
 
-	uniform_int_distribution<int> sample(0, 4);	//20%확률로 뽑아올 인덱스
 
 	for (int i = 0; i < n; i++)
 	{
 		ofstream fout;
 		string short_Name = directory + "\\shortRead_" + to_string(i) + ".txt";	//shortRead디렉토리에 저장
 		fout.open(short_Name);
-		//std::cout << dis(gen) << endl;
 
-		string temp = text.substr(dis(gen), length);	// 랜덤으로 뽑은 시작위치에서 length만큼의 서브스트링 반환
 
-		string* splited_temp = new string[length / 5];		// 20%확률로 돌연변이가 일어나기 위해 temp를 잘라서 만든 string배열
-		int size = length / 5;		// 예를 들어 length가 10이라면 splited_temp의 원소개수는 2개이다
+		string shortRead = myDNA.substr(dis(gen), length);	// 랜덤으로 뽑은 시작위치에서 length만큼의 서브스트링 반환
 
-		for (int i = 0; i < size; i++)
-		{
-			splited_temp[i] = temp.substr(i * (length / size), length / size);	// splited_temp에는 temp를 등분한 string이 저장됨 
-		}
 
-		//std::cout << temp << " " << splited_temp[0] << " " << splited_temp[1] << endl;
-
-		for (int i = 0; i < size; i++)			//splited_temp의 모든 원소. 즉 temp가 등분되어 만들어진 string에 대하여
-		{
-			int index = sample(gen);		// 20%확률로 랜덤하게 돌연변이가 일어날 인덱스 설정
-
-			if (splited_temp[i][index] == 'A')	// 돌연변이가 일어날 인덱스에 해당하는 문자가 A라면
-			{
-				char arr[3] = { 'G','C','T' };
-				splited_temp[i][index] = shuffle(arr, 3);	// G,C,T 중에서 하나로 바뀜
-			}
-			else if (splited_temp[i][index] == 'G')
-			{
-				char arr[3] = { 'A','C','T' };
-				splited_temp[i][index] = shuffle(arr, 3);
-
-			}
-			else if (splited_temp[i][index] == 'C')
-			{
-				char arr[3] = { 'A','G','T' };
-				splited_temp[i][index] = shuffle(arr, 3);
-
-			}
-			else if (splited_temp[i][index] == 'T')
-			{
-				char arr[3] = { 'A','G','C' };
-				splited_temp[i][index] = shuffle(arr, 3);
-
-			}
-		}
-
-		string shortRead = concat(splited_temp, size);	//	돌연변이가 일어난 결과. shortRead
-
-		//string* splited_Read = new string[size];
-		//for (int i = 0; i < size; i++)
-		//{
-		//	splited_Read[i] = shortRead.substr(i * (length / size), length / size);
-		//}
-
-		//std::cout << shortRead << " " << splited_Read[0] << " " << splited_Read[1] << endl;
-		//
-		//std::cout << "============================" << endl;
-		fout << shortRead << endl;
+		fout << shortRead;
 		fout.close();
 	}
 
@@ -302,9 +304,6 @@ int main()
 		information.close();
 
 	}
-
-
-
 
 
 
