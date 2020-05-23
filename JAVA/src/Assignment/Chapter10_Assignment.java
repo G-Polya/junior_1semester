e4074e2d87a7c839a4a7ba137818cb5fb0fc1ea3
G_Polya <wlskaka4@gmail.com>
2020-05-23 11:20:30 +0900

    no message

--- JAVA/src/Assignment/Chapter10_Assignment.java
+++ JAVA/src/Assignment/Chapter10_Assignment.java
@@ -4,51 +4,59 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Chapter10_Assignment extends JFrame {
-    public Chapter10_Assignment() {
-        super("+,- 키로 폰트 크기 조절");
+public class Chapter10_Assignment extends JFrame
+{
+    public Chapter10_Assignment()
+    {
+        super("위, 아래 방향키로 폰트 크기 조절");
+        // Frame과 Pane설정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
         c.setBackground(Color.WHITE);
 
-        JLabel label = new JLabel("Love Java");
+        JLabel label = new JLabel("Love Java"); //크기를 키울 대상
         c.add(label);
 
-        Font font = new Font("TimesRoman", Font.PLAIN, 14);
+        Font font = new Font("TimesRoman", Font.PLAIN, 14);     // 폰트 설정. 처음 크기는 14
+        // label.setFont(font);
 
-        label.setFont(font); // 14 픽셀 크기
-        label.addKeyListener(new KeyAdapter() {
+        // KeyAdapter와 Listener를 활용한 이벤트 설정
+        label.addKeyListener(new KeyAdapter()
+        {
             @Override
-            public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == 38) {		// 위쪽 누름
-                    JLabel la = (JLabel)e.getSource();
+            public void keyPressed(KeyEvent e)  // 키보드가 눌러지는 이벤트를 처리할 메소드
+            {
+                if(e.getKeyCode() == 38)    // 위쪽 방향키의 코드는 38이다
+                {
+                    JLabel la = (JLabel)e.getSource();  // 현재 label 객체 받아오기
                     Font f = la.getFont();
                     int size = f.getSize();
-                    if(size > 24)
+                    if(size >= 24)   // label의 크기가 24보다 크면
                     {
-                        c.setBackground(Color.CYAN);
+                        c.setBackground(Color.CYAN);    // 배경색을 하늘색으로 변경
                         return;
                     }
                     else
-                        c.setBackground(Color.WHITE);
+                        c.setBackground(Color.WHITE);  //아니면 그대로
 
-                    la.setFont(new Font("TimesRoman", Font.PLAIN, size+1));
+                    la.setFont(new Font("TimesRoman", Font.PLAIN, size+1)); //위 방향키 입력마다 1씩 사이즈가 증가된다
                 }
-                else if(e.getKeyCode() == 40) {		// 아래쪽 누름
+                else if(e.getKeyCode() == 40) 	// 아래쪽 코드는 40이다
+                {
                     JLabel la = (JLabel)e.getSource();
                     Font f = la.getFont();
                     int size = f.getSize();
-                    if(size < 8)
+                    if(size <= 8)    // label의 크기가 8보다 작으면
                     {
-                        c.setBackground(Color.YELLOW);
+                        c.setBackground(Color.YELLOW);  // 배경색을 노란색으로 변경
                         return;
                     }
                     else
                         c.setBackground(Color.WHITE);
 
 
-                    la.setFont(new Font("TimesRoman", Font.PLAIN, size-1));
+                    la.setFont(new Font("TimesRoman", Font.PLAIN, size-1)); // 아래 방향키 입력마다 1씩 사이즈가 감소한다
                 }
             }
         });
--- JAVA/src/Assignment/Chapter11_Assignment.java
+++ JAVA/src/Assignment/Chapter11_Assignment.java
@@ -3,15 +3,15 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-//http://blog.naver.com/PostView.nhn?blogId=ouo7581&logNo=221466625634&parentCategoryNo=&categoryNo=62&viewDate=&isShowPopularPosts=true&from=search
+
 public class Chapter11_Assignment extends JFrame
 {
     int[] unit = {50000,100000,1000,500,100,50,10,1};
     private String[] str = {"금액","50,000원","10,000원","1,000원","500원","100원","50원","10원","1원"};
     private JLabel la = new JLabel();
     private JTextField[] tf = new JTextField[str.length];
-    private JButton btn = new JButton("계산");
-    private JCheckBox[] checkbox = new JCheckBox[unit.length-1];
+    private JButton btn = new JButton("계산");    // 계산 버튼
+    private JCheckBox[] checkbox = new JCheckBox[unit.length-1];    // 화폐 선택
 
     public Chapter11_Assignment()
     {
@@ -21,25 +21,54 @@ public class Chapter11_Assignment extends JFrame
         c.setBackground(Color.PINK);
         c.setLayout(null);
 
-        la = new JLabel(str[0]);
+        // 금액을 입력할 부분. label과 TextField
+        la = new JLabel(str[0]);    //"금액" 라벨
         la.setSize(40,20);
         la.setLocation(30,20);
         c.add(la);
-
-        tf[0] = new JTextField(30);
+        tf[0] = new JTextField(30); // 금액이 입력될 textfield
         tf[0].setSize(100,20);
         tf[0].setLocation(90,20);
         c.add(tf[0]);
 
+        // 체크박스, JTextField, label생성
+        for(int i = 1; i< str.length;i++)
+        {
+            // label
+            la = new JLabel(str[i]);
+            la.setHorizontalAlignment(JLabel.RIGHT);
+            la.setSize(50,20);
+            la.setLocation(50,50+(i*20));
+            add(la);
+
+            // JTextField
+            tf[i] = new JTextField(30);
+            tf[i].setHorizontalAlignment(JTextField.CENTER);
+            tf[i].setSize(70,20);
+            tf[i].setLocation(120,50+(i*20));
+            add(tf[i]);
+
+            // JCheckbox
+            if(i < str.length-1)
+            {
+                checkbox[i-1] = new JCheckBox();
+                checkbox[i-1].setBackground(Color.PINK);
+                checkbox[i-1].setSize(20,20);
+                checkbox[i-1].setLocation(210,50+(i*20));
+                add(checkbox[i-1]);
+            }
+        }
+
+
         btn.setSize(70,20);
         btn.setLocation(200,20);
 
-        btn.addActionListener(new ActionListener()
+        btn.addActionListener(new ActionListener()  // 계산 버튼 이벤트 처리
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                String input = tf[0].getText();
+                String input = tf[0].getText(); //입력된 금액
                 if(input.length() == 0)
                     return;
 
@@ -55,45 +84,23 @@ public class Chapter11_Assignment extends JFrame
                         break;
                     }
 
-                    if(!checkbox[i].isSelected())       // 체크된거 확인
+                    if(!checkbox[i].isSelected())       // 체크된거 확인. 체크 안된거는 0으로. 체크된걸로만 거슬러준다
                     {
                         tf[i+1].setText("0");
                         continue;
                     }
 
-                    m = money/unit[i];
-                    tf[i+1].setText(Integer.toString(m));
+                    m = money/unit[i];      // 화폐의 개수. 65,500원에서 천원짜리로 거슬렀으면 65개를 거슬러준다
+                    tf[i+1].setText(Integer.toString(m));       // JTextField에 출력
                     if(m>0)
-                        money = money % unit[i];
+                        money = money % unit[i];    // 나머지 돈에 대한 처리를 위한 과정
                 }
             }
         });
 
         add(btn);
 
-        for(int i = 1; i< str.length;i++)
-        {
-            la = new JLabel(str[i]);
-            la.setHorizontalAlignment(JLabel.RIGHT);
-            la.setSize(50,20);
-            la.setLocation(50,50+(i*20));
-            add(la);
 
-            tf[i] = new JTextField(30);
-            tf[i].setHorizontalAlignment(JTextField.CENTER);
-            tf[i].setSize(70,20);
-            tf[i].setLocation(120,50+(i*20));
-            add(tf[i]);
-
-            if(i < str.length-1)
-            {
-                checkbox[i-1] = new JCheckBox();
-                checkbox[i-1].setBackground(Color.PINK);
-                checkbox[i-1].setSize(20,20);
-                checkbox[i-1].setLocation(210,50+(i*20));
-                add(checkbox[i-1]);
-            }
-        }
 
         setSize(300,300);
         setVisible(true);
