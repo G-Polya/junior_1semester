3c78291fe2e2490741d3138b98be8426572d9696
G_Polya <wlskaka4@gmail.com>
2020-06-08 14:16:02 +0900

    no message

--- JAVA/src/Assignment/Chapter14_Assignment_2.java
+++ JAVA/src/Assignment/Chapter14_Assignment_2.java
@@ -20,6 +20,10 @@ public class Chapter14_Assignment_2 extends JFrame
     private Clip[] audioClip = new Clip[4]; // 재생을 위한 클립객체
 
     String[] audio_name = {"audio/wolf.wav", "audio/dhol_drums.wav", "audio/sirenpolice.wav","audio/hiphop.wav"};   // 오디오파일 이름
+
+    String playing_audio[] = new String[4];    // 체크박스에서 선택된 wav파일의 이름들
+
+
     public Chapter14_Assignment_2()
     {
 
@@ -40,13 +44,12 @@ public class Chapter14_Assignment_2 extends JFrame
         JCheckBox[] checkBoxes = new JCheckBox[4]; // 체크박스 생성과 위치설정. container에 삽입
         for(int i = 0; i< checkBoxes.length; i++)
         {
-            checkBoxes[i] = new JCheckBox(audio_name[i]);
+            checkBoxes[i] = new JCheckBox(audio_name[i]);   //체크박스의 레이블은 audio_name이다.
             checkBoxes[i].setSize(400,20);
             checkBoxes[i].setLocation(120,50 + (i * 20));
             container.add(checkBoxes[i]);
 
         }
-
 
         // 액션리스너 객체생성. 익명클래스 이용
         ActionListener actionListener = new ActionListener()
@@ -54,30 +57,31 @@ public class Chapter14_Assignment_2 extends JFrame
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                switch (e.getActionCommand())
+                String command = e.getActionCommand();
+                if(command == "music start")        // music start누를때 노래를 시작
                 {
-                    case "music start":     // music
-                        count = 0;
-                        stop_flag = false;
+                    count = 0;
+                    stop_flag = false;
 
-                        for(int i = 0; i< checkBoxes.length; i++)
+                    for(int i = 0; i< checkBoxes.length; i++)   // 체크박스확인 후, 체크됬으면 selected_audio초기화.
+                    {
+                        if(checkBoxes[i].isSelected())
                         {
-                            if(checkBoxes[i].isSelected())
-                            {
-                                selected_audio[count++] = checkBoxes[i].getText();
-                            }
+                            playing_audio[count] = checkBoxes[i].getText();
+                            count++;    // 체크될때마다 count증가
                         }
-                        loadAudio(0);
-                        break;
-
-                    case "music end":
-                        audioClip[playing].stop();
-                        stop_flag = true;
-                        break;
+                    }
+                    loadAudio(0);
+                }
+                else if(command == "music end") // music end를 누르면 노래정지
+                {
+                    audioClip[playing].stop();
+                    stop_flag = true;
                 }
             }
         };
 
+        // 시작, 정지 버튼 생성과 이벤트처리
         JButton btn[] = {new JButton("music start"), new JButton("music end")};
         for(int i = 0; i < btn.length ;i++)
         {
@@ -93,17 +97,16 @@ public class Chapter14_Assignment_2 extends JFrame
 
     }
 
-    String selected_audio[] = new String[4];
 
     public void loadAudio(int index)
     {
-        if(index == count)
+        if(index == count)  // index와  count가 같다 >> 끝까지 연주했다.
         {
-            count = 0;
+            count = 0;  // 설정초기화
             int i = 0;
-            while(i<selected_audio.length)
+            while(i< playing_audio.length)
             {
-                selected_audio[i] = "";
+                playing_audio[i] = "";
                 audioClip[i].close();
                 i++;
             }
@@ -112,27 +115,27 @@ public class Chapter14_Assignment_2 extends JFrame
         {
             try
             {
-                File audios = new File(selected_audio[index]);
+                File audios = new File(playing_audio[index]);   //오디오파일 경로명
 
-                final AudioInputStream soundStream = AudioSystem.getAudioInputStream(audios);
+                final AudioInputStream soundStream = AudioSystem.getAudioInputStream(audios); // 오디오 파일로부터
 
-                audioClip[index] = AudioSystem.getClip();
+                audioClip[index] = AudioSystem.getClip();   // 비어있는 오디오클립만들기
 
                 playing = index;
 
-                audioClip[index].addLineListener(new LineListener()
+                audioClip[index].addLineListener(new LineListener() // 음원종료시의 동작
                 {
                     @Override
                     public void update(LineEvent event)
                     {
-                        if(event.getType() == LineEvent.Type.STOP)
+                        if(event.getType() == LineEvent.Type.STOP)  // clip.stop()이 호출되거나 재생이 끝났을떄
                         {
                             if(!stop_flag)
                             {
                                 try
                                 {
-                                    soundStream.close();
-                                    loadAudio(index+1);
+                                    soundStream.close(); // 오디오스트림 종료
+                                    loadAudio(index+1); // 다음 오디오파일 재생
                                 }
                                 catch (IOException err)
                                 {
@@ -143,8 +146,8 @@ public class Chapter14_Assignment_2 extends JFrame
                     }
                 });
 
-                audioClip[index].open(soundStream);
-                audioClip[index].start();
+                audioClip[index].open(soundStream); //재생할 오디오스트림 열기
+                audioClip[index].start();   //재생 시작
 
             }
             catch(Exception e)
